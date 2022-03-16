34ee00a9cd47930259b41d983ae78766b53b6122
iulian Preuteasa <iulian_preuteasa@yahoo.com>
2022-03-16 05:47:34 +0000

    working on navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import logo from "../../Images/logo.png";
+import "./navbar.css";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="nav">
+      <logo>
+        <img src={logo} />
+        <h3>Reddit Iulian</h3>
+      </logo>
+      <searchbar className="search">
+        <input type="search" id="searchBar" />
+        <button> Search</button>
+      </searchbar>
+      <dropdown className="dropdown"></dropdown>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
