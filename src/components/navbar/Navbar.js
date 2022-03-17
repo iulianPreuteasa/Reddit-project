3b7339f5067b03e100727eb67c6ef35946fd78da
iulian Preuteasa <iulian_preuteasa@yahoo.com>
2022-03-17 05:50:13 +0000

    Finnish CSS in nav section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,15 +5,23 @@ import "./navbar.css";
 const NavBar = () => {
   return (
     <div className="nav">
-      <logo>
-        <img src={logo} />
-        <h3>Reddit Iulian</h3>
+      <logo className="logoNav">
+        <img src={logo} className="logoImg"/>
+        <h4 className="titleNav">Reddit Iulian</h4>
       </logo>
       <searchbar className="search">
-        <input type="search" id="searchBar" />
-        <button> Search</button>
+        <input type="search" className="searchBar" id="searchBar" placeholder="Find an articole"/>
+        <button className="searchButton"> Search</button>
       </searchbar>
-      <dropdown className="dropdown"></dropdown>
+      <sideMenu className="sideMenu">
+        <h2>title 1</h2>
+        <h2>title 2</h2>
+        <h2>title 3</h2>
+        <h2>title 4</h2>
+        <h2>title 5</h2>
+        <h2>title 6</h2>
+        <h2>title 7</h2>
+      </sideMenu>
     </div>
   );
 };
