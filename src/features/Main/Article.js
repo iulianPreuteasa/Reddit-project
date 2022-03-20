4651b962dd93c2af8014ce2d3ec1fd204a315b87
iulian Preuteasa <iulian_preuteasa@yahoo.com>
2022-03-20 08:09:27 +0000

    stuck

--- src/features/Main/Article.js
+++ src/features/Main/Article.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 export const Article = (props) => {
+  const comments = `http://www.reddit.com${props.article.permalink}`;
   return (
     <>
       <div className="article">
         <h4 className="title">{props.article.title}</h4>
-        <img className="imgArticle" src={props.article.url} alt="" />
+        <img className="imgArticle" src={props.article.thumbnail} alt="" />
         <div className="comments">
-          <p className="comment"> {props.article.permalink}</p>
-
-          <button className="expandComments">Expand comments</button>
+          <a href={comments} target="_blank">
+            <button className="expandComments">Go to comments</button>
+          </a>
           <votes className="votes">
             <div> Likes: {props.article.ups}</div>
             <div>
